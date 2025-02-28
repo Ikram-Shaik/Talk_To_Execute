3f73a0acdc93f180a191d95dbeda7cbc436cfc9b
SHAIK MOHAMMED IKRAM <140187518+Ikram-Shaik@users.noreply.github.com>
2025-02-28 21:46:44 +0530

    Update gmail.py

--- Plugins/gmail.py
+++ Plugins/gmail.py
@@ -4,13 +4,13 @@ from email.message import EmailMessage
 def send_email(receiver_email, subject, body):
     try:
         email = EmailMessage()
-        email['From'] = "ikramshaik.cse.ai@gmail.com"
+        email['From'] = "EMAIL ID"
         email['To'] = receiver_email
         email['Subject'] = subject
         email.set_content(body)
 
         with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-            smtp.login("ikramshaik.cse.ai@gmail.com", "qimb mobd kixt hjve")  # Replace with your app password
+            smtp.login("EMAIL ID", "EMAIL PASSWORD")  # Replace with your app password
             smtp.send_message(email)
 
             print("✅ Email sent successfully!")
